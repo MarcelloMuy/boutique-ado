1408a67bad572a2e57fc81456cb4841ee4b26551
MarcelloMuy <marcellomuylaert@icloud.com>
2022-05-11 20:10:18 +0000

    Added checkout success logic

--- bag/contexts.py
+++ bag/contexts.py
@@ -5,7 +5,7 @@ from products.models import Product
 
 
 def bag_contents(request):
-    
+
     bag_items = []
     total = 0
     product_count = 0
@@ -32,16 +32,16 @@ def bag_contents(request):
                     'product': product,
                     'size': size,
                 })
-    
+
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
-        
+
     grand_total = delivery + total
-    
+
     context = {
         'bag_items': bag_items,
         'total': total,
@@ -51,5 +51,5 @@ def bag_contents(request):
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
-    
+
     return context
