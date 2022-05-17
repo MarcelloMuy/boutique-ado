5ec132dc7061b43ce1fa40bdf844ba591eb78036
MarcelloMuy <marcellomuylaert@icloud.com>
2022-05-17 05:32:14 +0000

    Added profile form, views and updated template

--- profiles/models.py
+++ profiles/models.py
@@ -12,13 +12,13 @@ class UserProfile(models.Model):
     delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    defaultphone_number = models.CharField(max_length=20, null=True, blank=True)
-    defaultcountry = CountryField(blank_label='Country *', null=True, blank=True)
-    defaultpostcode = models.CharField(max_length=20, null=True, blank=True)
-    defaulttown_or_city = models.CharField(max_length=40, null=True, blank=True)
-    defaultstreet_address1 = models.CharField(max_length=80, null=True, blank=True)
-    defaultstreet_address2 = models.CharField(max_length=80, null=True, blank=True)
-    defaultcounty = models.CharField(max_length=80, null=True, blank=True)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    default_county = models.CharField(max_length=80, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label='Country', null=True, blank=True)
 
     def __str__(self):
         return self.user.username
